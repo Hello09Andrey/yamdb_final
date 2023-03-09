6255e93dcc2de16661f55574e32a34caacdc17c2
Hello09Andrey <104667768+Hello09Andrey@users.noreply.github.com>
2023-03-09 07:10:19 +0200

    nothind

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -7,7 +7,9 @@ SECRET_KEY = 'p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs'
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [
+    '158.160.31.66',
+    ]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
